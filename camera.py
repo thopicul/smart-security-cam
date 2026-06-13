dfb9293861c9c578743cc83fa90b9d935375a6a4
Thopicul Hasan <thasan8000@gmail.com>
2026-06-13 11:25:18 -0400

    camera display on mac

--- camera.py
+++ camera.py
@@ -1,2 +1,25 @@
-print("hello world")
-print("testing")
+import cv2
+
+# Initlize the Webcam
+cap = cv2.VideoCapture(0)
+
+# check if the webcome is opened correctly
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+print("Press 'q' to close the video window.")
+
+while True:
+    # Capture frame by frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is true
+    if not ret:
+        print("Can't receive frame. exiting..")
+        break
+    cv2.imshow("'Macbook webcam Feed", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
